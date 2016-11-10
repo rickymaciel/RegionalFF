d821e1516c830a6e234f3e7a8c93964a7a47811d
rickymaciel <rmacielb3@gmail.com>
2016-11-10 10:57:09 -0300

    Se habilito las optimizaciones Gzip  bundletable

--- RegionalFF/Global.asax.cs
+++ RegionalFF/Global.asax.cs
@@ -16,6 +16,7 @@ namespace RegionalFF
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
