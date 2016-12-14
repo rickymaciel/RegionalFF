02faa0a977aed5ccc3d5e02720514894c311ef10
rickymaciel <rmacielb3@gmail.com>
2016-12-14 17:35:20 -0300

    Dashboard Facilitacion

--- RegionalFF/Controllers/HomeController.cs
+++ RegionalFF/Controllers/HomeController.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using RegionalFF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace RegionalFF.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult NotAuthorized()
-        {
-            return View();
-        }
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            ViewBag.CiudadId = new SelectList(db.Ciudads, "Id", "Nombre");
+            ViewBag.PaisId = new SelectList(db.Pais, "Id", "Nombre");
+            DateTime Fecha = DateTime.Now;
+            var UserId = User.Identity.GetUserId();
+            ViewBag.Fecha = Fecha;
             return View();
         }
     }
